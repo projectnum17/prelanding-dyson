1f6e9721d4b3e2ada8a66b217f6b176a1b44f2e3
projectnum17 <egor.chornyy@gmail.com>
2023-04-30 01:33:43 +0300

    feat: last commit

--- js/main.js
+++ js/main.js
@@ -1,72 +1,4 @@
-const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
-
-//Show input error messages
-function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
-}
-
-//show success colour
-function showSucces(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-}
-
-//check email is valid
-function checkEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(re.test(input.value.trim())) {
-        showSucces(input)
-    }else {
-        showError(input,'Email is not invalid');
-    }
-}
-
-
-//checkRequired fields
-function checkRequired(inputArr) {
-    inputArr.forEach(function(input){
-        if(input.value.trim() === ''){
-            showError(input,`${getFieldName(input)} is required`)
-        }else {
-            showSucces(input);
-        }
-    });
-}
-
-
-//check input Length
-function checkLength(input, min ,max) {
-    if(input.value.length < min) {
-        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
-    }else if(input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be les than ${max} characters`);
-    }else {
-        showSucces(input);
-    }
-}
-
-//get FieldName
-function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-//Event Listeners
-form.addEventListener('submit',function(e) {
-    e.preventDefault();
-
-    checkRequired([username, email]);
-    checkLength(username,3,15);
-    checkEmail(email);
-}); 
-
-// Отправка заявки 
+// Отправка 
 $(document).ready(function() {
 	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
 		if (document.form.name.value == '' || document.form.email.value == '' ) {
@@ -74,8 +6,8 @@ $(document).ready(function() {
 			return valid;
 		}
 		$.ajax({
-			type: "POST",
-			url: "mail/mail.php",
+			/* type: "POST",
+			url: "mail/mail.php", */
 			data: $(this).serialize()
 		}).done(function() {
 			$('.js-overlay-thank-you').fadeIn();
@@ -86,7 +18,7 @@ $(document).ready(function() {
 	});
 });
 
-// Закрыть попап «спасибо»
+// Закрыть
 $('.js-close-thank-you').click(function() { // по клику на крестик
 	$('.js-overlay-thank-you').fadeOut();
 });
