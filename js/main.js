88e49b2a8647626c86b7a2215d74b208112112cc
projectnum17 <egor.chornyy@gmail.com>
2023-04-30 09:00:16 +0300

    README

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Отправка 
 $(document).ready(function() {
-	$('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+	$('#form').submit(function() { // перевірка на пусті поля. Атрибут html5 — required не підходить (не підпримується Safari)
 		if (document.form.name.value == '' || document.form.email.value == '' ) {
 			valid = false;
 			return valid;
@@ -19,11 +19,11 @@ $(document).ready(function() {
 });
 
 // Закрыть
-$('.js-close-thank-you').click(function() { // по клику на крестик
+$('.js-close-thank-you').click(function() { // крестик
 	$('.js-overlay-thank-you').fadeOut();
 });
 
-$(document).mouseup(function (e) { // по клику вне попапа
+$(document).mouseup(function (e) { // за зоною попАпа
 	var popup = $('.popup');
 	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
 		$('.js-overlay-thank-you').fadeOut();
