eceaa29b6f88b2ec9606276ec47fb726869d09c0
projectnum17 <egor.chornyy@gmail.com>
2023-04-30 09:41:55 +0300

    feat: preloader

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-// Отправка 
-$(document).ready(function() {
-	$('#form').submit(function() { // перевірка на пусті поля. Атрибут html5 — required не підходить (не підпримується Safari)
-		if (document.form.name.value == '' || document.form.email.value == '' ) {
+//loader
+
+$(window).ready(function () {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+});
+
+// відправка 
+$(document).ready(function () {
+	$('#form').submit(function () { // перевірка на пусті поля. Атрибут html5 — required не підходить (не підпримується Safari)
+		if (document.form.name.value == '' || document.form.email.value == '') {
 			valid = false;
 			return valid;
 		}
@@ -9,7 +16,7 @@ $(document).ready(function() {
 			/* type: "POST",
 			url: "mail/mail.php", */
 			data: $(this).serialize()
-		}).done(function() {
+		}).done(function () {
 			$('.js-overlay-thank-you').fadeIn();
 			$(this).find('input').val('');
 			$('#form').trigger('reset');
@@ -18,14 +25,14 @@ $(document).ready(function() {
 	});
 });
 
-// Закрыть
-$('.js-close-thank-you').click(function() { // крестик
+// Закрити
+$('.js-close-thank-you').click(function () { // крестик
 	$('.js-overlay-thank-you').fadeOut();
 });
 
 $(document).mouseup(function (e) { // за зоною попАпа
 	var popup = $('.popup');
-	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+	if (e.target != popup[0] && popup.has(e.target).length === 0) {
 		$('.js-overlay-thank-you').fadeOut();
 	}
 });
